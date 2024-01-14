27ef426626bfb068d8165bd138aa469174da7589
FanambNyAvo <144690663+FanambNyAvo@users.noreply.github.com>
2024-01-14 04:20:53 +0300

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,58 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+
+            int guessCount = 0;
+
+
+            while (true)
+            {
+
+                Console.Write("What is your guess? ");
+                int userGuess = Convert.ToInt32(Console.ReadLine());
+
+
+                guessCount++;
+
+
+                if (userGuess == magicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    break;
+                }
+                else if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+
+
+            Console.WriteLine($"It took you {guessCount} guesses.");
+
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            string playAgainInput = Console.ReadLine().ToLower();
+
+
+            if (playAgainInput != "yes")
+            {
+                playAgain = false;
+            }
+
+            Console.Clear(); 
+        }
     }
 }
