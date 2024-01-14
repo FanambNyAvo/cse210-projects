6ab49c2ccedc60bf0bc80bf5e6ec391abd4f70b5
FanambNyAvo <144690663+FanambNyAvo@users.noreply.github.com>
2024-01-14 04:19:35 +0300

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,71 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("Enter your grade percentage: ");
+        int gradePercentage = Convert.ToInt32(Console.ReadLine());
+
+
+        string letter;
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine($"Congratulations! You passed with a grade of {letter}.");
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, you did not pass. Better luck next time!");
+        }
+
+
+        int lastDigit = gradePercentage % 10;
+        string sign = "";
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3 && gradePercentage != 100)
+        {
+            sign = "-";
+        }
+
+
+        if (letter == "A" && sign == "+")
+        {
+            letter = "A-";
+            sign = "";
+        }
+        else if (letter == "F" && (sign == "+" || sign == "-"))
+        {
+            letter = "F";
+            sign = "";
+        }
+
+
+        Console.WriteLine($"Your final grade is {letter}{sign}");
     }
 }
